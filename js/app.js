6e75dede9c003b4f3a352a4a8aeb7efb7fb58416
Kannan G <gkannan271120@gmail.com>
2023-07-04 23:19:09 +0530

    Back door tour company

---
+++ js/app.js
@@ -0,0 +1,25 @@
+const navBtn = document.getElementById("nav-toggle");
+const links = document.getElementById("nav-links");
+
+navBtn.addEventListener("click", () => {
+  links.classList.toggle("show-links");
+});
+
+const scrollLinks = document.querySelectorAll(".scroll-link");
+scrollLinks.forEach((link) => {
+  link.addEventListener("click", (e) => {
+    e.preventDefault();
+    links.classList.remove("show-links");
+
+    const id = e.target.getAttribute("href").slice(1);
+    const element = document.getElementById(id);
+
+    let position = element.offsetTop - 62;
+
+    window.scrollTo({
+      left: 0,
+      top: position,
+      behaviour: "smooth",
+    });
+  });
+});
